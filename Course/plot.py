214a10058cc7f7484bffa9da2873a7d46519de45
Muhammed M.Bassem <Muhammed.m.bassem@gmail.com>
2014-10-27 11:20:14 +0200

    Editing

---
+++ Course/plot.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+
+def me(word,wordlist=None):
+	current = word[0]
+	for letter in word[1:]:
+
+		if (current > letter):
+			return False
+		elif 
+			
+		else 
+			
+			current = letter
+	
+		
+
+me("ddd")	
+	
+	
